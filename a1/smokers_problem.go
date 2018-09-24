72794841e091c5050e7b6738b34cbd86719709a8
Spencer Rose <boutrous@w134-87-176-092.wireless.uvic.ca>
2018-09-24 15:41:51 -0700

    Added Golang soln to Smokers Problem

--- a1/smokers_problem.go
+++ a1/smokers_problem.go
@@ -1,8 +1,18 @@
 // =========================================
-// Example 1: The Smokers Problem (Exercise 4.5)
+// Example 1: Cigarette Smokers Problem (Exercise 4.5)
 // =========================================
 // Completed for CSC 564 (Concurrency), Prof. Yvonne Coady, Fall 2018
 // Spencer Rose (ID V00124060)
+// =========================================
+// SUMMARY: Four threads are involved: an agent and three smokers. The smokers loop
+// forever, first waiting for ingredients, then making and smoking cigarettes. The
+// ingredients are tobacco, paper, and matches. We assume that the agent has an infinite
+// supply of all three ingredients, and each smoker has an infinite supply of one of the
+// ingredients; that is, one smoker has matches, another has paper, and the third has
+// tobacco. The agent repeatedly chooses two different ingredients at random and makes
+// them available to the smokers. Depending on which ingredients are chosen, the smoker
+// with the complementary ingredient should pick up both resources and proceed.
+// Reference: Downey, Allen B., The Little Book of Semaphores,  Version 2.2.1, pp 101-111.
 
 package main
 
@@ -10,24 +20,6 @@ import (
 	"sync"
 )
 
-// Smokers loop forever, first waiting for ingredients, then making and
-// smoking cigarettes. The ingredients are tobacco, paper, and matches.
-
-func smoker (ingredient string, tobacco chan string, paper chan string,
-			matches chan string, loop chan bool, wg *sync.WaitGroup) {
-	defer wg.Done()
-
-	// Pick unprovided ingredients
-	switch ingredient {
-	case "tobacco":
-		waitForIngredients(paper, matches, loop)
-	case "paper":
-		waitForIngredients(tobacco, matches, loop)
-	case "matches":
-		waitForIngredients(tobacco, paper, loop)
-	}
-
-}
 
 // Customer requests a haircut; if no chairs are available in the waiting room
 // s/he leaves the barbershop.
@@ -37,56 +29,53 @@ func agent (tobacco chan string, paper chan string, matches chan string, loop ch
 		for i := 1; i <= 3; i++ {
 			switch i {
 			case 1:
-				//fmt.Printf("agent puts out tobacco and paper\n")
+				// Agent puts out tobacco and paper
 				tobacco <- "tobacco"
 				paper <- "paper"
 			case 2:
-				//fmt.Printf("agent puts out paper and matches\n")
+				// Agent puts out paper and matches
 				paper <- "paper"
 				matches <- "matches"
 			case 3:
-				//fmt.Printf("agent puts out tobacco and matches\n")
+				// Agent puts out tobacco and matches
 				tobacco <- "tobacco"
 				matches <- "matches"
 			}
-			// Wait for smoker to finish
+			// Wait for a smoker to finish, then loop
 			<-loop
 		}
 	}
 }
 
 func waitForIngredients(ingr1Ch chan string, ingr2Ch chan string, loop chan bool) {
-	// smoker waits for other two ingredients
 	for {
-
+		// Smoker waits for other two ingredients
 		select {
 		case ingr1 := <-ingr1Ch:
-			//fmt.Printf("smoker has %s.\n", ingr1)
+			// Needed ingredient acquired; acquire third ingredient
 			select {
 			case <-ingr2Ch:
-				//fmt.Printf("smoker with has all ingredients.\n")
+				// Smoker has all the ingredients to smoke
 				makeCigarette()
 				smokeCigarette()
 				loop <- true
 				continue
 			default:
-				// Replace ingredient 1
-				//fmt.Printf("Replace ingredient %s.\n", ingr1)
+				// Replace ingredient since third not available
 				ingr1Ch <- ingr1
 				continue
 			}
 		case ingr2 := <-ingr2Ch:
-			//fmt.Printf("smoker has %s.\n", ingr2)
+			// Needed ingredient acquired; acquire third ingredient
 			select {
 			case <-ingr1Ch:
-				//fmt.Printf("smoker with has all ingredients.\n")
+				// Smoker has all the ingredients to smoke
 				makeCigarette()
 				smokeCigarette()
 				loop <- true
 				continue
 			default:
-				// Replace ingredient 2
-				//fmt.Printf("Replace ingredient %s.\n", ingr2)
+				// Replace ingredient since third not available
 				ingr2Ch <-ingr2
 				continue
 			}
@@ -106,25 +95,34 @@ func smokeCigarette() {
 
 func main() {
 
-	// Buffered channels for cigarette ingredients
+	// Channels to "transfer" cigarette ingredients
 	tobacco := make(chan string)
 	paper := make(chan string)
 	matches := make(chan string)
+	// Channel to notify agent when smoker has taken two ingredients
 	loop := make(chan bool)
 
 	var wg sync.WaitGroup
 	wg.Add(4)
 
-	//fmt.Printf("Begin agent thread\n")
-
 	// Agent thread
 	go agent(tobacco, paper, matches, loop, &wg)
 	// time.Sleep(1000 * time.Millisecond)
 
-	// Smoker threads
-	go smoker("tobacco", tobacco, paper, matches, loop, &wg)
-	go smoker("paper", tobacco, paper, matches, loop, &wg)
-	go smoker("matches", tobacco, paper, matches, loop, &wg)
+	// Smokers threads loop forever, first waiting for ingredients, then making and
+	// smoking cigarettes. The ingredients are tobacco, paper, and matches.
+	go func() {
+		defer wg.Done()
+		waitForIngredients(tobacco, paper, loop)
+	}()
+	go func() {
+		defer wg.Done()
+		waitForIngredients(tobacco, matches, loop)
+	}()
+	go func() {
+		defer wg.Done()
+		waitForIngredients(paper, matches, loop)
+	}()
 
 	wg.Wait()
 }
