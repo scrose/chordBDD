e3c7c2585ced207a117f9ab16de21dbdddbc23b1
Spencer Rose <boutrous@Spencers-MacBook-Pro-2.local>
2018-11-24 15:16:05 -0800

    Files for Part 3 (Chord DHT) of Assignment 2

---
+++ a2/chord_dht/Main.scala
@@ -0,0 +1,21 @@
+import ChordDHT.Chord
+import org.apache.log4j.{Level, Logger}
+
+object Main extends App {
+
+  // Disable logging messages
+  Logger.getLogger("org").setLevel(Level.OFF)
+  Logger.getLogger("akka").setLevel(Level.OFF)
+
+  // Create new Chord DHT
+  var dhtable = new Chord(3)
+
+  // Insert value into Chord Table
+  val value = List(0, 3, 6, 18, 27)
+  val key = dhtable.insert(value)
+  println(f"Key/Value created: ($key, $value)")
+
+  // Lookup value at key
+  println(f"Value Retrieved: ${dhtable.lookup(key).getOrElse("Not Found")}")
+
+}
